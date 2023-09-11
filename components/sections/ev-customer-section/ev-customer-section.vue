9b56aa846a3543d3e24793cf3a0011d2b3dd9e14
Anton Keller <ToniWebDev@mail.ru>
2023-09-11 11:43:45 +0300

    fix styles

--- components/sections/ev-customer-section/ev-customer-section.vue
+++ components/sections/ev-customer-section/ev-customer-section.vue
@@ -14,7 +14,7 @@
           «Крупнейшие компании России» (рейтинг Эксперт-400),
           совокупная выручка которых за 2008 год превысила 9.3 трлн. руб.
         </p>
-        <p class="border-l-4 border-l-gray-600/60 bg-red-100 p-2.5">
+        <p class="border-l-4 border-l-gray-600 bg-red-100 p-2.5 rounded-lg">
           Основным деловым преимуществом нашей компании является высокий профессионализм сотрудников, а также опыт
           выполнения проектов в сфере оценки и консалтинга с последующим согласованием результатов с крупнейшими
           международными аудиторскими компаниями.
@@ -38,18 +38,6 @@
           molestias qui quia quisquam repellendus voluptatibus. Ad deserunt dolorem earum eos esse eveniet incidunt iste
           laudantium libero magni maxime, minima, neque nesciunt nulla placeat porro, quae qui quia quos repellat soluta
           temporibus tenetur velit vero voluptate? Aliquid aut deserunt dolor dolore eaque esse, est facere illo impedit
-          inventore ipsa iusto laudantium magnam magni mollitia natus nemo nisi nostrum obcaecati odio officiis possimus
-          provident quaerat quasi qui quisquam quod quos reiciendis sint suscipit tempora temporibus ullam unde
-          veritatis voluptatem, voluptatibus voluptatum. Blanditiis eos, esse exercitationem iure neque obcaecati ullam!
-          Ab accusamus architecto, aut commodi corporis delectus, dolorem enim eos esse et eum expedita fugiat in ipsum
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae eligendi expedita impedit ipsa
-          molestias qui quia quisquam repellendus voluptatibus. Ad deserunt dolorem earum eos esse eveniet incidunt iste
-          laudantium libero magni maxime, minima, neque nesciunt nulla placeat porro, quae qui quia quos repellat soluta
-          temporibus tenetur velit vero voluptate? Aliquid aut deserunt dolor dolore eaque esse, est facere illo impedit
-          inventore ipsa iusto laudantium magnam magni mollitia natus nemo nisi nostrum obcaecati odio officiis possimus
-          provident quaerat quasi qui quisquam quod quos reiciendis sint suscipit tempora temporibus ullam unde
-          veritatis voluptatem, voluptatibus voluptatum. Blanditiis eos, esse exercitationem iure neque obcaecati ullam!
-          Ab accusamus architecto, aut commodi corporis delectus, dolorem enim eos esse et eum expedita fugiat in ipsum
         </p>
       </div>
 
@@ -66,36 +54,35 @@
           </div>
         </div>
       </div>
-
-
     </div>
 
-    <!--    <div class="carousel-xs">-->
-    <!--      <carousel-->
-    <!--        paginationSize=6-->
-    <!--        pagination-padding=4-->
-    <!--        paginationColor="rgb(243 244 246)"-->
-    <!--        paginationActiveColor="rgb(220 38 38)"-->
-    <!--        autoplay-->
-    <!--        autoplay-timeout=4000-->
-    <!--        :per-page="1"-->
-    <!--        :mouse-drag="false"-->
-    <!--      >-->
-    <!--        <slide v-for="element of experienceGroup" :key="element.id" class="flex flex-col justify-evenly">-->
-    <!--          <img class="mx-auto" :src="require(`../../../assets/experience_logotypes/${element.img}.png`)" alt="err">-->
-    <!--          <div class="flex flex-wrap justify-center pb-2 gap-1 mt-2 text-xs">-->
-    <!--              <span-->
-    <!--                class="px-2 py-0.5 rounded-xl font-medium border border-gray-700/60 bg-gray-100 backdrop-blur-sm text-gray-800 font-sans"-->
-    <!--                v-for="currService of element.services"-->
-    <!--                :key="currService.id"-->
-    <!--              >-->
-    <!--                {{ currService.title }}-->
-    <!--              </span>-->
-    <!--          </div>-->
+    <div class="carousel-xs">
+      <carousel
+        paginationSize=6
+        pagination-padding=4
+        paginationColor="rgb(243 244 246)"
+        paginationActiveColor="rgb(220 38 38)"
+        autoplay
+        autoplay-timeout=4000
+        :per-page="1"
+        :mouse-drag="false"
+      >
+        <slide v-for="element of experienceGroup" :key="element.id" class="flex flex-col justify-evenly">
+          <img class="mx-auto w-full max-w-[14rem]"
+               :src="require(`../../../assets/experience_logotypes/${element.img}.png`)" alt="err">
+          <div class="flex flex-wrap justify-center  gap-1  text-xs">
+                  <span
+                    class="px-2 py-0.5 rounded-xl font-medium border border-gray-700/60 bg-gray-100 backdrop-blur-sm text-gray-800 font-sans"
+                    v-for="currService of element.services"
+                    :key="currService.id"
+                  >
+                    {{ currService.title }}
+                  </span>
+          </div>
 
-    <!--        </slide>-->
-    <!--      </carousel>-->
-    <!--    </div>-->
+        </slide>
+      </carousel>
+    </div>
 
   </div>
 </template>
@@ -133,7 +120,7 @@ export default {
 }
 
 *::-webkit-scrollbar-track {
-  @apply bg-red-50;
+  @apply bg-transparent;
 }
 
 *::-webkit-scrollbar-thumb {
@@ -142,14 +129,6 @@ export default {
 
 p {
   @apply text-gray-800 text-sm md:text-base font-sans;
-}
-
-
-
-// ......................................
-
-.span-selected-red {
-  //@apply bg-red-900 text-gray-200 mx-0.5 px-1 py-0.5 rounded-md;
 }
 
 .customer-section {
@@ -169,11 +148,11 @@ p {
     @apply space-y-3 lg:space-y-5;
 
     h2 {
-      @apply text-gray-800 text-xl sm:text-2xl md:text-2xl lg:text-4xl font-serif;
+      @apply text-gray-800 text-xl sm:text-2xl md:text-2xl lg:text-4xl font-bold;
     }
 
     p {
-      @apply text-lg font-serif;
+      @apply text-base lg:text-lg;
     }
   }
 
@@ -184,11 +163,11 @@ p {
     @apply lg:h-[70vh] lg:h-[70vh];
 
     .info-block-wrapper {
-      @apply overflow-y-scroll;
+      @apply lg:overflow-y-scroll;
       @apply text-center md:text-left align-top;
       @apply space-y-[calc(3vw+8px)] sm:space-y-6 md:space-y-8 lg:space-y-10;
       @apply lg:w-[56vw] xl:w-[50vw] lg:max-w-[750px];
-      @apply lg:h-full pr-2;
+      @apply lg:h-full  lg:pr-2;
     }
 
     .experience-box {
@@ -197,23 +176,34 @@ p {
       @apply hidden md:flex flex-wrap justify-evenly items-stretch;
       @apply bg-gray-50 text-center;
       @apply md:gap-3 lg:gap-2;
-      @apply border-t border-t-gray-400 lg:border-t-gray-100 rounded-md;
+      @apply border-t border-t-gray-100 lg:border-t-gray-50 rounded-md;
+      @apply duration-300;
 
       & * {
         @apply text-gray-200 text-xs md:text-sm;
       }
 
       .experience-element {
-        @apply bg-gray-900 rounded-md;
+        @apply flex flex-col justify-around;
+        @apply bg-gray-800/95 rounded-xl;
+        @apply min-h-[215px];
         @apply md:w-[34vw] lg:w-[23.5vw] xl:w-[17.5vw] xl:max-w-[280px] py-4;
+
+        &:hover {
+          background: #ad0202;
+        }
+
+        &:hover .experience-element--services .service {
+          @apply bg-gray-100 text-gray-900;
+        }
 
         .experience-element--services {
           @apply flex flex-wrap justify-center;
           @apply gap-1 text-xs sm:text-xs;
 
           .service {
-            @apply rounded-xl bg-red-900 py-0.5 px-1.5;
-            @apply font-sans text-gray-200;
+            @apply rounded-xl bg-gray-800/80 border-gray-900/95 border py-0.5 px-1.5;
+            @apply text-gray-200;
           }
         }
       }
@@ -221,7 +211,10 @@ p {
   }
 
   .carousel-xs {
-    @apply md:hidden max-w-full text-center bg-gray-800 rounded-md;
+    @apply md:hidden max-w-full text-center bg-gray-800/95 rounded-xl mt-6;
+    @apply min-h-[170px] sm:min-h-[190px];
+    //background: #ad0202;
+    //@apply bg-red-800/90;
   }
 }
 
--- components/widgets/ev-footer.vue
+++ components/widgets/ev-footer.vue
@@ -1,50 +1,61 @@
 <template>
   <footer class="footer-container">
-
-    <div class="w-full flex flex-col space-y-3.5 items-center sm:items-start border-b border-b-gray-700/95">
-
-      <div class="flex flex-col items-center sm:block">
-        <img class="icon" src="../../assets/icons/address-icon.png" alt="no img">
-        <span class="mr-1">125040, Россия, Москва, 3-я улица Ямского поля, д.2 корп.7, офис 301</span>
+    <div class="elements">
+      <div class="element" v-for="el of companyInfo.elements">
+        <img class="e-icon" :src="require(`../../assets/icons/${el.img}`)" alt="no img">
+        <div class="e-text">{{ el.text }}</div>
       </div>
-
-      <div class="flex flex-col items-center sm:block">
-        <img class="icon" src="../../assets/icons/phone-icon.png" alt="no img">
-        <span>+7 (495) 717 01 01</span>
-        <span>+7 (499) 557 07 97</span>
-      </div>
-
-      <div class="flex flex-col items-center sm:block">
-        <img class="icon" src="../../assets/icons/email-icon.png" alt="no img">
-        <span>info@evcons.ru</span>
-      </div>
-
     </div>
-
-    <div class="w-full text-left py-8">
-      ©2023 "ООО Эверест Консалтиг"
+    <div class="copyright">
+      Copyright © 2023, ООО Эверест Консалтиг, все права защищены
     </div>
-
   </footer>
 </template>
 
 <script>
 export default {
-  name: "ev-footer"
+  name: "ev-footer",
+  data() {
+    return {
+      companyInfo: {
+        elements: [
+          {img: "phone-icon.png", text: "+7 495 717-01-01 +7 499 557-07-97"},
+          {img: "email-icon.png", text: "info@evcons.ru"},
+          {img: "address-icon.png", text: "125040, Россия, Москва, 3-я улица Ямского поля, д.2 корп.7, офис 301"},
+        ],
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 .footer-container {
-  @apply w-full bg-gray-900;
-  @apply flex flex-col;
-  @apply text-center text-gray-200 font-light font-sans text-base;
-  @apply p-4 sm:p-6 md:p-12 lg:p-14 xl:p-16;
-}
+  @apply w-full flex flex-col bg-gray-900 text-gray-100 text-base text-center sm:text-left;
+  @apply px-[calc(8vw-6px)] sm:px-[12vw] md:px-[12vw] lg:px-10 xl:px-12;
+  @apply py-6 sm:py-8 md:py-10 lg:py-12 xl:py-14;
 
-.icon {
-  @apply inline-block w-5 h-5 sm:w-6 sm:h-6 block sm:inline mr-2;
+  .elements {
+    @apply w-full flex flex-col items-center space-y-6 sm:space-y-8 pb-4  sm:items-start border-b border-b-gray-700/95;
+
+    .element {
+      @apply flex flex-col sm:flex-row items-center space-y-2.5 sm:space-y-0 sm:space-x-3;
+
+      .e-icon {
+        @apply w-[24px] h-[24px];
+      }
+
+      .e-text {
+        @apply font-sans;
+      }
+    }
+  }
+
+  .copyright {
+    @apply w-full mt-6 font-sans text-center sm:text-left;
+  }
+
 }
 
 </style>
